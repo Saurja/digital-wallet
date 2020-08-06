a4aa8b605d81e470b6795b16ceef9a826e9192d8
“Saurja” <saurjaghoshofficial@gmail.com>
2020-08-06 23:14:19 +0530

    Added Npm, Bootstrap

---
+++ includes/bootstrap.php
@@ -0,0 +1,7 @@
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS. -->
+<script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
+<script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
--- index.php
+++ index.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Digital Wallet</title>
+
+    <?php include("includes/bootstrap.php"); ?>
+
 </head>
-<body>
-    
-</body>
+    <body>
+        
+    </body>
 </html>
