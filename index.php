d40f0cce7479ccb8973251cc6794c892b2d12fc1
“Saurja” <saurjaghoshofficial@gmail.com>
2020-08-07 11:09:24 +0530

    Index UI Complete

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Digital Wallet</title>
-
-    <?php include("includes/bootstrap.php"); ?>
-
-</head>
-    <body>
-        
-    </body>
-</html>
+<?php include("includes/header.php"); ?>
+    <h2 class="pageHeadingBig">Welcome to Your<br> Digital Wallet</h2>
+<?php include("includes/footer.php"); ?>
