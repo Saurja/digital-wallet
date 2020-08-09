adf0894808ea68033b32abec3eab8a5ae0d001fd
“Saurja” <saurjaghoshofficial@gmail.com>
2020-08-09 17:15:34 +0530

    Completed : Login Procss

--- includes/handlers/login-handler.php
+++ includes/handlers/login-handler.php
@@ -2,12 +2,14 @@
     //login button was pressed
     if (isset($_POST['loginButton'])) {
         $email = $_POST['loginEmail'];
-    }
-
+   
     //login function
     $loginSuccessful = $account->login($email);
 
     if($loginSuccessful) {
         header("Location: index.php");
     }
+}
 ?>
+
+    
