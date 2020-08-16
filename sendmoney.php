195ebafd21253bf48deb4614d01f15576e9a2a05
“Saurja” <saurjaghoshofficial@gmail.com>
2020-08-16 11:19:37 +0530

    Listing Done

--- sendmoney.php
+++ sendmoney.php
@@ -65,7 +65,7 @@ if(isset($_POST['sendMoneyButton'])){
         <table class="table table-bordered mt-2">
             <thead>
                 <tr>
-                    <th scope="col">#</th>
+                    <th scope="col">Req ID</th>
                     <th scope="col">Who</th>
                     <th scope="col">Amount</th>
                     <th scope="col">Req-Date</th>
@@ -73,13 +73,44 @@ if(isset($_POST['sendMoneyButton'])){
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>From user_A@exapmple.com</td>
-                    <td>20 Points Requested</td>
-                    <td>2020-01-23</td>
-                    <td><button type="button" class="btn btn-dark">Pay Now</button></td>
-                </tr>
+
+            <!--    List All Requests   -->
+            <?php
+            
+                $sender = $_SESSION['userLoggedIn'];
+                $reqCreditQuery = mysqli_query($con, "SELECT * FROM credit_requests WHERE send_from='$sender'");
+                while($row = mysqli_fetch_array($reqCreditQuery)) {
+
+                    $rowID = $row['req_id'];
+                    echo "<tr>
+                            <th scope='row'>" . $row['req_id'] . "</th>
+                            <td>From " . $row['req_from'] .  "</td>
+                            <td>" . $row['credits_requested'] .  " Points Requested</td>
+                            <td>" . $row['req_dateTime'] .  "</td>
+                            <td><button type='submit' name='sendRequestedMoneyButton" . $rowID . "' class='btn btn-dark'>Pay Now</button></td>
+                        </tr>";
+                }
+
+                while($row = mysqli_fetch_array($reqCreditQuery)) {
+
+                    $rowID = $row['req_id'];
+                    
+                    if(isset($_POST['sendRequestedMoneyButton'.$rowID])){
+
+                        $sender = $_SESSION['userLoggedIn'];
+                        $receiverName= $row['req_from'];
+                        $amount = $row['credits_requested'];
+                        
+                        $wasSuccessful = $transactions->sendcredits($sender, $receiverName, $amount);
+
+                        if($wasSuccessful){
+                            echo $sender . " ";
+                            echo $receiver . " ";
+                            echo $amount . " ";
+                        }
+                    }
+                }
+            ?>
             </tbody>
         </table>
     </div>
