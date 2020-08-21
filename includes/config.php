fc58c809737b0ccb21299eb5493fa897034daddd
“Saurja” <saurjaghoshofficial@gmail.com>
2020-08-21 22:18:22 +0530

    Bug Fixed: Connection Not Ending

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,11 @@
     ob_start();
     session_start();
     $timezone = date_default_timezone_set("Asia/Kolkata");
-    $con = mysqli_connect("sql7.freemysqlhosting.net", "sql7361593", "PLbzbqNpzK", "sql7361593");
+    try {
+        $con = mysqli_connect("sql7.freemysqlhosting.net", "sql7361593", "PLbzbqNpzK", "sql7361593");
+    } catch (Exception $e) {
+        die("Unable to connect: " . $e->getMessage());
+    }
     if(mysqli_connect_errno()) {
         echo "Failed to Connect : " . mysqli_connect_errno() ;
     }
--- logout.php
+++ logout.php
@@ -1,6 +1,7 @@
 <?php
 
     session_start();
+    mysqli_close($con); 
     unset($_SESSION['userLoggedIn']);
     header("Location: register.php");
 
