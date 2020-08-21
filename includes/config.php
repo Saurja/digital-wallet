1e9a4eaeec287a4e3923889bc8ec8825db773306
“Saurja” <saurjaghoshofficial@gmail.com>
2020-08-21 15:22:27 +0530

    Database Connected

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
     ob_start();
     session_start();
     $timezone = date_default_timezone_set("Asia/Kolkata");
-    $con = mysqli_connect("localhost", "root", "", "digital-wallet");
+    $con = mysqli_connect("sql7.freemysqlhosting.net", "sql7361593", "PLbzbqNpzK", "sql7361593");
     if(mysqli_connect_errno()) {
         echo "Failed to Connect : " . mysqli_connect_errno() ;
     }
--- includes/transactionConfig.php
+++ includes/transactionConfig.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $dbh = new PDO('mysql:host=localhost;dbname=digital-wallet','root','');
+    $dbh = new PDO('mysql:host=sql7.freemysqlhosting.net;dbname=sql7361593','sql7361593','PLbzbqNpzK');
 } catch (Exception $e) {
     die("Unable to connect: " . $e->getMessage());
 }
