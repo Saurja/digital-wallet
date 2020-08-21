ddb203bc849aa0816d25a89bdfa6db26b8f4fecb
“Saurja” <saurjaghoshofficial@gmail.com>
2020-08-21 10:54:22 +0530

    Config changes

--- includes/transactionConfig.php
+++ includes/transactionConfig.php
@@ -1,11 +1,7 @@
 <?php
-
-#   Config for making a transaction
-
 try {
     $dbh = new PDO('mysql:host=localhost;dbname=digital-wallet','root','');
 } catch (Exception $e) {
     die("Unable to connect: " . $e->getMessage());
 }
-
 ?>
