d8de235bf876827dd935f6a20358baa635cf0f8c
“Saurja” <saurjaghoshofficial@gmail.com>
2020-08-22 09:14:27 +0530

    Local

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
     session_start();
     $timezone = date_default_timezone_set("Asia/Kolkata");
     try {
-        $con = mysqli_connect("sql7.freemysqlhosting.net", "sql7361593", "PLbzbqNpzK", "sql7361593");
+        $con = mysqli_connect("localhost", "root", "", "digital-wallet");
     } catch (Exception $e) {
         die("Unable to connect: " . $e->getMessage());
     }
