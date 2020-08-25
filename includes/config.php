6a7cfe7ef9fdbf65676b22e8be2fee36a4f0764d
“Saurja” <saurjaghoshofficial@gmail.com>
2020-08-25 22:05:16 +0530

    Switched Localhost

--- includes/config.php
+++ includes/config.php
@@ -3,8 +3,8 @@
     session_start();
     $timezone = date_default_timezone_set("Asia/Kolkata");
     try {
-        $con = mysqli_connect("sql7.freemysqlhosting.net", "sql7361593", "PLbzbqNpzK", "sql7361593");
-        #$con = mysqli_connect("localhost", "root", "", "digital-wallet");
+        #$con = mysqli_connect("sql7.freemysqlhosting.net", "sql7361593", "PLbzbqNpzK", "sql7361593");
+        $con = mysqli_connect("localhost", "root", "", "digital-wallet");
     } catch (Exception $e) {
         die("Unable to connect: " . $e->getMessage());
     }
